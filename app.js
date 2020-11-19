5c0a3fc522159c89f5bc46a1ea52903a2acac8db
Simon <simonpham1294@gmail.com>
2020-11-19 17:54:34 -0400

    new changes

--- app.js
+++ app.js
@@ -72,7 +72,21 @@ function displayModal(index) {
     const modalHTML = `
         <img class="avatar" src="${picture.large}" />
         <div class="text-container">
-            <h2 class="name"><span id ="back">&lt;</span>${name.first} ${name.last}<span id = "forward">&gt;</span></h2> 
+            <h2 class="name">
+                <svg version="1.1" id="back" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                    viewBox="0 0 490.667 490.667" style="enable-background:new 0 0 490.667 490.667;" xml:space="preserve"><path d="M239.077,245.333L466.213,18.219c4.171-4.16,4.179-10.914,0.019-15.085
+                    c-2.006-2.011-4.731-3.139-7.571-3.134h-192c-2.831-0.005-5.548,1.115-7.552,3.115L24.443,237.781
+                    c-4.164,4.165-4.164,10.917,0,15.083l234.667,234.667c2.001,2.007,4.718,3.135,7.552,3.136h192
+                    c5.891,0.011,10.675-4.757,10.686-10.648c0.005-2.84-1.123-5.565-3.134-7.571L239.077,245.333z"/><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g>
+                 </svg>
+                <span id ="h2-text">${name.first} ${name.last}</span>
+                 <svg version="1.1" id="forward" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                    viewBox="0 0 490.667 490.667" style="enable-background:new 0 0 490.667 490.667;" xml:space="preserve"><path  d="M466.219,237.781L231.552,3.115C229.548,1.115,226.831-0.005,224,0H32
+                    c-5.891-0.011-10.675,4.757-10.686,10.648c-0.005,2.84,1.123,5.565,3.134,7.571l227.136,227.115L24.448,472.448
+                    c-4.171,4.16-4.179,10.914-0.019,15.085c2.006,2.011,4.731,3.139,7.571,3.134h192c2.831,0.005,5.548-1.115,7.552-3.115
+                    l234.667-234.667c4.171-4.16,4.179-10.914,0.019-15.085C466.231,237.794,466.225,237.788,466.219,237.781z"/><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g>
+                </svg>
+            </h2> 
             <p class="email">${email}</p>
             <p class="address">${city}</p>
             <hr />
@@ -91,13 +105,13 @@ function displayModal(index) {
     const forward = document.getElementById('forward'); 
 
     back.addEventListener('click', (e) =>{
-        overlay.classList.add('hidden'); 
-        displayModal(index-1);
+        overlay.classList.add('hidden');
+        displayModal(parseInt(index) - 1);
     }); 
 
     forward.addEventListener('click', (e) =>{
         overlay.classList.add('hidden'); 
-        displayModal(index+1);
+        displayModal(parseInt(index) + 1);
     });
 };
 
